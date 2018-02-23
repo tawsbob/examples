72e54939a956bdb2ae1a20ec515fc75957fa6815
Dellean Santos <dellean.santos@gmail.com>
2018-02-22 23:54:28 -0300

    primisse example 1

---
+++ js/promisses/promisses-example-1.js
@@ -0,0 +1,20 @@
+      //ES5
+
+    let idade = 20
+
+    const minhaTarefa = new Promise((resolve,reject)=>{
+        if (typeof idade == 'number') {
+          resolve(true)
+        } else {
+           var motivoDoErro = new Error('Idade inválida');
+            reject(motivoDoErro);
+        }
+    })
+
+    //TESTANDO A PROMISSE
+    minhaTarefa.then((idadeValida)=>{
+      console.log(idadeValida)
+      //TRUE
+    }).catch((error)=>{
+      console.log(error)
+    })
